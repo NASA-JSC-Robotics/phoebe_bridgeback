f3b097328e99594c1702bc6b648d3a2b5dab2be5
Jodi Graf <jodi.s.graf@nasa.gov>
2024-12-09 15:55:01 -0600

    Removed the controller manager from control.launch.py

--- phoebe_deploy/launch/control.launch.py
+++ phoebe_deploy/launch/control.launch.py
@@ -31,6 +31,7 @@
 # Redistribution and use in source and binary forms, with or without
 # modification, is not permitted without the express permission
 # of Clearpath Robotics.
+import os
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, GroupAction, OpaqueFunction
 from launch.conditions import IfCondition, UnlessCondition
@@ -57,15 +58,27 @@ REMAPPINGS = [
 def generate_launch_description():
     
     
+
+    pkg_deploy = get_package_share_directory('phoebe_deploy')
+    pkg_description = get_package_share_directory('phoebe_description')
+    
+    robot_controllers = PathJoinSubstitution(
+        [
+            FindPackageShare("phoebe_deploy"),
+            "config",
+            "config.yaml",
+        ]
+    )
+    urdf_model_path = os.path.join(pkg_description, 'urdf/phoebe.urdf.xacro')
     
     controller_manager = Node(
-        name='controller_manager',
         package='controller_manager',
-        namespace='phoebe',
+#        namespace='phoebe',
         executable='ros2_control_node',
-        arguments=['--controller-manager-timeout', '60', "joint_state_broadcaster"],
+        parameters=[robot_controllers],
         output='screen',
-        additional_env={'ROS_SUPER_CLIENT': 'True'},
+        remappings=REMAPPINGS,
+
  #       condition=IfCondition(arg_use_sim_time),
     )
      # Add Joint State Broadcaster
@@ -84,9 +97,18 @@ def generate_launch_description():
         output='screen',
         additional_env={'ROS_SUPER_CLIENT': 'True'},
     )
+    # Add Platform Velocity Controller
+#    platform_velocity_controller = Node(
+#        package='controller_manager',
+#        executable='spawner',
+#        parameters=[urdf_model_path, robot_controllers],
+#        arguments=['--controller-manager-timeout', '60', 'platform_velocity_controller'],
+#        output='screen',
+#        additional_env={'ROS_SUPER_CLIENT': 'True'},
+#    )
 
     ld = LaunchDescription()
-    ld.add_action(controller_manager)
+#    ld.add_action(controller_manager)
     ld.add_action(joint_state_broadcaster)
     ld.add_action(platform_velocity_controller)
     return ld
