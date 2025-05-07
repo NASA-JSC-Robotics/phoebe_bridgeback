48125a2f241ab4f61c5baf46b3c5b11c1f3870e6
Nathan Dunkelberger <nathan.b.dunkelberger@nasa.gov>
2025-05-07 10:30:56 -0500

    adding transport mode

---
+++ phoebe_deploy/launch/transport/transport_control.launch.py
@@ -0,0 +1,169 @@
+#!/usr/bin/env python3
+
+import os
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, OpaqueFunction, GroupAction
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from ament_index_python.packages import get_package_share_directory
+from launch_ros.actions import Node, PushRosNamespace
+from launch_ros.parameter_descriptions import ParameterFile
+from launch.conditions import IfCondition
+
+
+def launch_setup(context, *args, **kwargs):
+
+    # Initialize Arguments
+    platform = LaunchConfiguration("platform")
+    tf_prefix = LaunchConfiguration("tf_prefix")
+    ns = LaunchConfiguration("ns")
+
+    # convert platform type to string so that we can evaluate different options
+    platform_string = platform.perform(context)
+    sim_ignition = "true" if platform_string == "sim_ignition" else "false"
+    use_fake_hardware = "true" if platform_string == "mock_hardware" else "false"
+
+    # common launch args shared across different nodes
+    common_launch_args = {
+        "sim_ignition": sim_ignition,
+        "use_fake_hardware": use_fake_hardware,
+        "tf_prefix": tf_prefix,
+        "ns": ns,
+    }.items()
+
+    # helper function to organize launch description objects with the same launch args and package names
+    def AddLaunchDescriptions(package_name, launch_file_names, launch_args, if_condition="true"):
+        launch_files_list = []
+        for launch_file_name in launch_file_names:
+            launch_files_list.append(
+                IncludeLaunchDescription(
+                    PythonLaunchDescriptionSource(
+                        os.path.join(get_package_share_directory(package_name), "launch", launch_file_name)
+                    ),
+                    launch_arguments=launch_args,
+                    condition=IfCondition(if_condition),
+                )
+            )
+
+        return launch_files_list
+
+    # helper function to make controller nodes
+    def MakeControllerNode(controller_name):
+        return Node(
+            package="controller_manager",
+            executable="spawner",
+            name=controller_name,
+            arguments=[
+                "--controller-manager",
+                "controller_manager",
+                "--controller-manager-timeout",
+                "300",
+                "--namespace",
+                ns,
+                controller_name,
+            ],
+            output="screen",
+        )
+
+    # lists to keep track of launch file names to start
+    launch_file_names = []
+
+    # This is the "definitive" robot state publisher.
+    # This should be launched on whatever machine has the most resources, which
+    # along with whichever controller manager we think should com up first.
+    launch_file_names.append("robot_state_publisher.launch.py")
+
+    # generate the launch files based on launch_file_names which has been configured
+    launch_files = AddLaunchDescriptions(
+        package_name="phoebe_deploy",
+        launch_file_names=launch_file_names,
+        launch_args=common_launch_args,
+    )
+
+    # helper function to get controllers files that we might need
+    def GetControllersFile(file_name):
+        return PathJoinSubstitution(
+            [
+                get_package_share_directory("phoebe_deploy"),
+                "config",
+                file_name,
+            ]
+        )
+
+    # launch controller manager
+
+    # contains update rate
+    controllers_common = GetControllersFile("controllers_common.yaml")
+    # controllers for the ridgeback
+    controllers_r100 = GetControllersFile("controllers_r100.yaml")
+
+    nodes = []
+
+    # start the controller manager node with all of the controller config files
+    control_node = Node(
+        package="controller_manager",
+        executable="ros2_control_node",
+        namespace=ns,
+        # allow_substs allows tf_prefix to be pulled in
+        parameters=[
+            ParameterFile(controllers_common, allow_substs=True),
+            ParameterFile(controllers_r100, allow_substs=True),
+        ],
+        remappings=[
+            # remap to be able to use the global robot_description
+            ("~/robot_description", "robot_description"),
+            # Necessary remap for platform velocity controller. Preferably this would be done
+            # at spawn time. This is not supported in humble, but is supported in jazzy.
+        ],
+        output="both",
+    )
+
+    node_puma_throttle = Node(
+        name="puma_throttle",
+        executable="throttle",
+        package="topic_tools",
+        namespace=ns,
+        output="screen",
+        arguments=["messages", "platform/puma/cmd", "50", "ridgeback/platform/puma/cmd_throttle"],
+    )
+
+    platform_velocity_controller = MakeControllerNode("platform_velocity_controller")
+    joint_state_broadcaster = MakeControllerNode("joint_state_broadcaster")
+
+    nodes = [control_node, node_puma_throttle, platform_velocity_controller, joint_state_broadcaster]
+
+    ns_action = GroupAction(actions=[PushRosNamespace(ns)] + launch_files + nodes)
+
+    return [ns_action]
+
+
+def generate_launch_description():
+
+    declared_arguments = []
+
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "platform",
+            default_value="hardware",
+            description="Whether to run the robot on hardware, mock_hardware, or sim_ignition.",
+            choices=["hardware", "mock_hardware", "sim_ignition"],
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "tf_prefix",
+            default_value="",
+            description="tf_prefix of the joint names, useful for \
+        multi-robot setup. If changed, also joint names in the controllers' configuration \
+        have to be updated.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "ns",
+            default_value="",
+            description="Namespace for the hardware robot",
+        )
+    )
+
+    return LaunchDescription(declared_arguments + [OpaqueFunction(function=launch_setup)])
---
+++ phoebe_deploy/launch/transport/transport_robot_state_publisher.launch.py
@@ -0,0 +1,83 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import (
+    Command,
+    FindExecutable,
+    LaunchConfiguration,
+    PathJoinSubstitution,
+)
+from launch_ros.actions import Node
+from launch_ros.parameter_descriptions import ParameterValue
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+
+    declared_arguments = []
+
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "ns",
+            default_value="",
+            description="Namespace for the robot.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "tf_prefix",
+            default_value="",
+            description="tf prefix for the robot joints.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "sim_ignition",
+            default_value="false",
+            description="Load the robot with ignition simulation description.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "use_fake_hardware",
+            default_value="true",
+            description="Start robot with simulated hardware mirroring command to its states.",
+        )
+    )
+
+    ns = LaunchConfiguration("ns")
+    tf_prefix = LaunchConfiguration("tf_prefix")
+    sim_ignition = LaunchConfiguration("sim_ignition")
+    use_fake_hardware = LaunchConfiguration("use_fake_hardware")
+
+    # main robot description for Phoebe
+    robot_description_content = Command(
+        [
+            PathJoinSubstitution([FindExecutable(name="xacro")]),
+            " ",
+            PathJoinSubstitution([FindPackageShare("phoebe_description"), "urdf", "r100", "r100.urdf.xacro"]),
+            " ",
+            "ns:=",
+            ns,
+            " ",
+            "tf_prefix:=",
+            tf_prefix,
+            " ",
+            "sim_ignition:=",
+            sim_ignition,
+            " ",
+            "use_fake_hardware:=",
+            use_fake_hardware,
+            " ",
+        ]
+    )
+    robot_description = {"robot_description": ParameterValue(value=robot_description_content, value_type=str)}
+
+    robot_state_publisher_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        namespace=ns,
+        output="both",
+        parameters=[robot_description],
+    )
+
+    return LaunchDescription(declared_arguments + [robot_state_publisher_node])
