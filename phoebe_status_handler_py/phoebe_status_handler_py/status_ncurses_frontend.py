bda80c82ed23c4a60605109b257f7847440de8ab
Mark Paterson <mark.paterson@nasa.gov>
2025-05-12 21:38:23 +0000

    WIP: Connect to real topics

--- phoebe_status_handler_py/phoebe_status_handler_py/status_ncurses_frontend.py
+++ phoebe_status_handler_py/phoebe_status_handler_py/status_ncurses_frontend.py
@@ -1,29 +1,32 @@
 import curses
-import sys
-from status_state import StatusState
+from phoebe_status_handler_py.status_state import StatusState
 
-class StringObject(object):
-    def __init__(self, screen: curses.window, row: int, col: int, content:str=""):
+
+class StrStatus(object):
+    def __init__(self, screen: curses.window, row: int, col: int, content: str = ""):
         self.screen = screen
         self.row = row
         self.col = col
         self.content = content
-    
+
     def draw(self, attributes):
         self.screen.addstr(self.row, self.col, self.content, attributes)
 
-class WheelStringObject(StringObject):
+
+class WheelStatus(StrStatus):
     def __init__(self, screen, row, col):
         super().__init__(screen, row, col)
 
     def draw(self, upper_attributes, lower_attributes=None, value=" "):
         self.screen.addstr(self.row, self.col, value, upper_attributes)
-        self.screen.addstr(self.row, self.col+2, value, lower_attributes if lower_attributes else upper_attributes)
+        self.screen.addstr(self.row, self.col+2, value,
+                           lower_attributes if lower_attributes else upper_attributes)
+
 
 class StatusNcursesFrontend(object):
     body = ("-----------------------",
             "|                      \\",
-            "|                       | front",
+            "|                       |",
             "|                      /",
             "-----------------------"
             )
@@ -47,7 +50,7 @@ class StatusNcursesFrontend(object):
         self.colors = {
             "grey":         (curses.color_pair(1) | curses.A_DIM),
             "white":        (curses.color_pair(1) | curses.A_BOLD),
-            "green":        curses.color_pair(2),            
+            "green":        curses.color_pair(2),
             "red":          curses.color_pair(3),
             "blue":         curses.color_pair(4),
             "black":        curses.color_pair(5),
@@ -55,13 +58,21 @@ class StatusNcursesFrontend(object):
 
         }
 
+        # Shorthands for the bulkier but more descriptive names
+        start_row = self.BODY_START_ROW
+        end_row = self.BODY_END_ROW
+        start_col = self.BODY_START_COL
+        end_col = self.BODY_END_COL
+
         self.status_objects = {
-            "leftRear":   WheelStringObject(self.screen_, self.BODY_START_ROW-1, self.BODY_START_COL-1),
-            "leftFront":  WheelStringObject(self.screen_, self.BODY_START_ROW-1, self.BODY_END_COL-2),
-            "rightRear":  WheelStringObject(self.screen_, self.BODY_END_ROW,   self.BODY_START_COL-1),
-            "rightFront": WheelStringObject(self.screen_, self.BODY_END_ROW,   self.BODY_END_COL-2),
-            "battery":    StringObject(self.screen_, self.BODY_START_ROW+1, self.BODY_START_COL+2, " BAT "),
-            "charging":   StringObject(self.screen_, self.BODY_START_ROW+3, self.BODY_START_COL+2, " CHG "),
+            "leftRear":   WheelStatus(self.screen_, start_row-1, start_col-1),
+            "leftFront":  WheelStatus(self.screen_, start_row-1, end_col-2),
+            "rightRear":  WheelStatus(self.screen_, end_row,     start_col-1),
+            "rightFront": WheelStatus(self.screen_, end_row,     end_col-2),
+            "battery":      StrStatus(self.screen_, start_row+1, start_col+2, " BAT "),
+            "charging":     StrStatus(self.screen_, start_row+3, start_col+2, " CHG "),
+            "drivingLeft":  StrStatus(self.screen_, start_row-1, end_col+3,   "-->"),
+            "drivingRight": StrStatus(self.screen_, end_row,     end_col+3,   "-->")
         }
 
         self.logger = None
@@ -72,18 +83,21 @@ class StatusNcursesFrontend(object):
         self.screen_.clear()
 
         # Draw title
-        self.screen_.addstr(0, 10, "Phoebe Status", self.colors["grey"] | curses.A_UNDERLINE)
+        self.screen_.addstr(0, 10, "Phoebe Status",
+                            self.colors["grey"] | curses.A_UNDERLINE)
 
         # Draw body
         row = self.BODY_START_ROW
         for line in self.body:
-            self.screen_.addstr(row, self.BODY_START_COL, line, self.colors["grey"])
+            self.screen_.addstr(row, self.BODY_START_COL,
+                                line, self.colors["grey"])
             row += 1
-        
+
         # Draw status objects
         self.update_all_corner_lighting("grey")
         self.update_battery("grey")
         self.update_charging_state("grey")
+        self.update_driving_state("black")
         self.screen_.refresh()
 
     def set_logger(self, logger):
@@ -94,19 +108,25 @@ class StatusNcursesFrontend(object):
             return self.colors["black"]
         return attributes
 
-
     def update_battery(self, color: str, is_blinking=False):
-        attributes = self.fix_attr(self.colors[color] | curses.A_REVERSE, is_blinking)
+        attributes = self.fix_attr(
+            self.colors[color] | curses.A_REVERSE, is_blinking)
         self.status_objects["battery"].draw(attributes)
 
     def update_charging_state(self, color: str, is_blinking=False):
-        attributes = self.fix_attr(self.colors[color] | curses.A_REVERSE, is_blinking)
+        attributes = self.fix_attr(
+            self.colors[color] | curses.A_REVERSE, is_blinking)
         self.status_objects["charging"].draw(attributes)
 
-    def update_corner_lighting(self, 
-                               which_corner: str, 
-                               upper_color: str, 
-                               lower_color: str=None, 
+    def update_driving_state(self, color: str, is_blinking=False):
+        attributes = self.fix_attr(self.colors[color], is_blinking)
+        self.status_objects["drivingLeft"].draw(attributes)
+        self.status_objects["drivingRight"].draw(attributes)
+
+    def update_corner_lighting(self,
+                               which_corner: str,
+                               upper_color: str,
+                               lower_color: str = None,
                                is_blinking=False,
                                content=" "):
         if which_corner not in self.status_objects:
@@ -114,15 +134,26 @@ class StatusNcursesFrontend(object):
 
         if lower_color is None:
             lower_color = upper_color
-        upper_attributes = self.fix_attr(self.colors[upper_color] | curses.A_REVERSE, is_blinking)
-        lower_attributes = self.fix_attr(self.colors[lower_color] | curses.A_REVERSE, is_blinking)
-        self.status_objects[which_corner].draw(upper_attributes, lower_attributes, content)
+        upper_attributes = self.fix_attr(
+            self.colors[upper_color] | curses.A_REVERSE, is_blinking)
+        lower_attributes = self.fix_attr(
+            self.colors[lower_color] | curses.A_REVERSE, is_blinking)
+        self.status_objects[which_corner].draw(
+            upper_attributes, lower_attributes, content)
 
-    def update_all_corner_lighting(self, upper_color, lower_color=None, is_blinking=False, content=" "):
-        self.update_corner_lighting("leftRear",   upper_color, lower_color, is_blinking, content)
-        self.update_corner_lighting("leftFront",  upper_color, lower_color, is_blinking, content)
-        self.update_corner_lighting("rightRear",  upper_color, lower_color, is_blinking, content)
-        self.update_corner_lighting("rightFront", upper_color, lower_color, is_blinking, content)
+    def update_all_corner_lighting(self,
+                                   upper_color,
+                                   lower_color=None,
+                                   is_blinking=False,
+                                   content=" "):
+        self.update_corner_lighting(
+            "leftRear",   upper_color, lower_color, is_blinking, content)
+        self.update_corner_lighting(
+            "leftFront",  upper_color, lower_color, is_blinking, content)
+        self.update_corner_lighting(
+            "rightRear",  upper_color, lower_color, is_blinking, content)
+        self.update_corner_lighting(
+            "rightFront", upper_color, lower_color, is_blinking, content)
 
     def update(self, state: StatusState):
         self.update_count += 1
@@ -134,21 +165,24 @@ class StatusNcursesFrontend(object):
         elif state.estop_state == StatusState.ROBOT_STATE_ESTOPPED:
             self.update_all_corner_lighting("red", is_blinking=True)
         elif state.estop_state == StatusState.ROBOT_STATE_NEEDS_RESET:
-            self.update_all_corner_lighting("red", is_blinking=True, content="R")
+            self.update_all_corner_lighting(
+                "red", is_blinking=True, content="R")
         elif state.estop_state == StatusState.ROBOT_STATE_RUNNING:
-            self.update_corner_lighting("leftRear", "red")            
-            self.update_corner_lighting("rightRear", "red")            
-            self.update_corner_lighting("leftFront", "white")            
+            self.update_corner_lighting("leftRear", "red")
+            self.update_corner_lighting("rightRear", "red")
+            self.update_corner_lighting("leftFront", "white")
             self.update_corner_lighting("rightFront", "white")
         else:
             self.update_all_corner_lighting("grey")
 
         if state.battery_state == StatusState.BATTERY_STATE_LOW:
-            self.update_battery("yellow", is_blinking=False)
+            self.update_battery("yellow")
         elif state.battery_state == StatusState.BATTERY_STATE_OK:
             self.update_battery("green")
         elif state.battery_state == StatusState.BATTERY_STATE_FULL:
             self.update_battery("blue")
+        elif state.battery_state == StatusState.BATTERY_STATE_FAULTED:
+            self.update_battery("red")
         else:
             self.update_battery("grey")
 
@@ -157,7 +191,12 @@ class StatusNcursesFrontend(object):
         elif state.charging_state == StatusState.CHARGING_STATE_ACTIVE:
             self.update_charging_state("green")
         else:
-            self.update_charging_state("grey")            
+            self.update_charging_state("grey")
+
+        if state.driving_state == StatusState.DRIVING_STATE_ON:
+            self.update_driving_state("green", is_blinking=True)
+        else:
+            self.update_driving_state("black")
 
         self.screen_.refresh()
 
--- phoebe_status_handler_py/phoebe_status_handler_py/status_state.py
+++ phoebe_status_handler_py/phoebe_status_handler_py/status_state.py
@@ -1,19 +1,32 @@
+# Holder to gather high-level state
+# This could easily be converted to a message if we desired, and
+# possibly for logging that would be nice
 class StatusState(object):
+    # Robot stop state
     ROBOT_STATE_OFF = 0
     ROBOT_STATE_NO_COMM = 1
     ROBOT_STATE_ESTOPPED = 2
     ROBOT_STATE_NEEDS_RESET = 3
     ROBOT_STATE_RUNNING = 4
 
+    # Battery state
     BATTERY_STATE_OFF = 0
-    BATTERY_STATE_OK  = 1
+    BATTERY_STATE_OK = 1
     BATTERY_STATE_LOW = 2
     BATTERY_STATE_FULL = 3
+    BATTERY_STATE_FAULTED = 4
 
+    # Charging state
     CHARGING_STATE_OFF = 0
     CHARGING_STATE_ACTIVE = 1
+    CHARGING_STATE_FAULT_SHORE_POWER = 2
+
+    # Driving state
+    DRIVING_STATE_OFF = 0
+    DRIVING_STATE_ON = 1
 
     def __init__(self):
-        self.estop_state    = self.ROBOT_STATE_OFF
-        self.battery_state  = self.BATTERY_STATE_OFF
+        self.estop_state = self.ROBOT_STATE_OFF
+        self.battery_state = self.BATTERY_STATE_OFF
         self.charging_state = self.CHARGING_STATE_OFF
+        self.driving_state = self.DRIVING_STATE_OFF
