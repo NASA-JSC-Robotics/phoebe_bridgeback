e14b3e356ff3717e8dce98121cec145d139ef41c
Nathan Dunkelberger <nathan.b.dunkelberger@nasa.gov>
2025-05-19 15:09:42 -0500

    adding monitoring of controller manager

--- phoebe_safety/phoebe_safety/estop_manager.py
+++ phoebe_safety/phoebe_safety/estop_manager.py
@@ -32,8 +32,6 @@ class MessageMonitor:
         self.last_msg_time = self.node.get_clock().now()
 
     def get_status(self):
-        if self.name == "restart":
-            print(self.status)
         # if we haven't gotten a mssage yet, return the initial state
         if self.last_msg_time is None:
             return self.status
@@ -41,7 +39,6 @@ class MessageMonitor:
         # if we have timed out, return the timeout state
         current_time = self.node.get_clock().now()
         if (current_time - self.last_msg_time) > rclpy.duration.Duration(seconds=self.valid_time):
-            self.node.get_logger().info(f"timeout for {self.name}")
             return self.timeout_state
         # otherwise, return the actual state
         else:
@@ -52,7 +49,7 @@ class PhoebeEstopManager(Node):
     def __init__(self):
         super().__init__("phoebe_estop_manager")
 
-        self.robot_state = RobotState.RUNNING
+        self.robot_state = RobotState.ESTOP
 
         # Use ROS 2 parameter system
         self.freedrive_controller_name = (
@@ -66,16 +63,17 @@ class PhoebeEstopManager(Node):
         self.manager_cbg = MutuallyExclusiveCallbackGroup()
         self.freedrive_cbg = MutuallyExclusiveCallbackGroup()
         self.restart_cbg = MutuallyExclusiveCallbackGroup()
+        self.cm_monitor_cbg = MutuallyExclusiveCallbackGroup()
 
         self.rate_hz = 5
 
         self.stopped_controllers = []
 
-        self.is_estopped = False
+        self.is_estopped = True
         self.estop_message_monitor = MessageMonitor(
             name="estop",
             node=self,
-            initial_state=False,
+            initial_state=True,
             valid_time_seconds=2,
             timeout_state=True,
         )
@@ -114,26 +112,23 @@ class PhoebeEstopManager(Node):
         self.list_controllers_client = self.create_client(
             ListControllers, "controller_manager/list_controllers", callback_group=self.main_cbg
         )
-        # wait until all required services are available from controller_manager
-        self.get_logger().info("Waiting for list controllers service to come up on controller_manager/list_controllers")
-        self.list_controllers_client.wait_for_service()
-        self.get_logger().info("Service available")
 
         # Create client for controller manager service
         self.switch_controllers_client = self.create_client(
             SwitchController, "controller_manager/switch_controller", callback_group=self.main_cbg
         )
-        # wait until all required services are available from controller_manager
-        self.get_logger().info(
-            "Waiting for switch controller service to come up on controller_manager/switch_controller"
-        )
-        self.switch_controllers_client.wait_for_service()
-        self.get_logger().info("Service available")
 
         self.request_freedrive_mode_server = self.create_service(
             Trigger, "~/request_freedrive_mode", self.request_freedrive_mode
         )
         self.request_restart_server = self.create_service(Trigger, "~/request_restart", self.request_restart)
+
+        self.controller_manager_check_timer = self.create_timer(1 / self.rate_hz, self.controller_manager_check, callback_group=self.cm_monitor_cbg)
+        self.is_cm_active = False
+
+        self.is_first_activate = True
+        self.ready_to_start = False
+        self.last_cm_msg_warn_time = None
 
         # Logger
         self.get_logger().info(f"This node has started: {self.get_name()}")
@@ -166,7 +161,6 @@ class PhoebeEstopManager(Node):
 
     def request_restart(self, request, response):
         self.restart_message_monitor.set_status(True)
-        print(self.restart_message_monitor.get_status())
 
         timeout_seconds = 2.0
         timeout = rclpy.duration.Duration(seconds=timeout_seconds)
@@ -188,11 +182,15 @@ class PhoebeEstopManager(Node):
         Publishes current safety status and updates light indicators.
         """
 
+        # wait until we have queried the status of the controller manager
+        while not self.ready_to_start:
+            pass
+
         is_estopped = self.estop_message_monitor.get_status()
         is_freedrive_mode_requested = self.freedrive_message_monitor.get_status()
         is_restart_requested = self.restart_message_monitor.get_status()
 
-        self.get_logger().info(f"Robot State: {self.robot_state}")
+        self.get_logger().info(f"Robot State: {self.robot_state}", throttle_duration_sec=5.0)
 
         if self.robot_state == RobotState.RUNNING:
             # if we are estopped, set the state to estop
@@ -209,21 +207,30 @@ class PhoebeEstopManager(Node):
                 self.stop_controllers(save_stopped_controllers=True)
 
         else:
-            # always try to disable can and stop controllers
-            self.disable_can()
-            self.stop_controllers(save_stopped_controllers=False)
+            # if it is our first rodeo, we don't want to have to wait for restart requested 
+            # to switch to running mode
+            if self.is_first_activate:
+                if not is_estopped:
+                    self.is_first_activate = False
+                    self.robot_state = RobotState.RUNNING
 
-            # always check for estop status, in case we were in freedrive mode
-            if is_estopped:
-                self.robot_state = RobotState.ESTOP
+            # if it isn't our first rodeo, handle controllers like normal
+            else:
+                # always try to disable can and stop controllers
+                self.disable_can()
+                self.stop_controllers(save_stopped_controllers=False)
 
-            # if restart is requested, and we aren't estopped restart controllers and enable can
-            if is_restart_requested and not is_estopped:
-                self.restart_controllers()
-                self.enable_can()
-                self.robot_state = RobotState.RUNNING
-                # reset the status back to false,
-                self.restart_message_monitor.set_status(False)
+                # always check for estop status, in case we were in freedrive mode
+                if is_estopped:
+                    self.robot_state = RobotState.ESTOP
+
+                # if restart is requested, and we aren't estopped restart controllers and enable can
+                if is_restart_requested and not is_estopped:
+                    self.restart_controllers()
+                    self.enable_can()
+                    self.robot_state = RobotState.RUNNING
+                    # reset the status back to false,
+                    self.restart_message_monitor.set_status(False)
 
         self.publish_hb_msg()
 
@@ -244,6 +251,8 @@ class PhoebeEstopManager(Node):
         some controllers may be turned back on in some cases, like if a trajectory is accidentally
         performed while the robot is disabled
         """
+        if not self.is_cm_active:
+            return
 
         # request to get switch controllers, list controllers
         switch_controller_request = SwitchController.Request()
@@ -254,6 +263,8 @@ class PhoebeEstopManager(Node):
         switch_controller_request.timeout.nanosec = int(1.0e9)  # 0.5s
 
         # list the controllers that are running
+        if not self.list_controllers_client.wait_for_service(0.05):
+            return
         list_controllers_response = self.list_controllers_client.call(list_controllers_request)
 
         # each new time this is called, we will repopulate which controllers we are stopping
@@ -274,6 +285,8 @@ class PhoebeEstopManager(Node):
         # if there are any to stop, call switch_controllers to stop them
         if controllers_to_stop:
             switch_controller_request.deactivate_controllers = controllers_to_stop
+            if not self.switch_controllers_client.wait_for_service(0.1):
+                return
             switch_controllers_response = self.switch_controllers_client.call(switch_controller_request)
             if not switch_controllers_response.ok:
                 self.get_logger().error("Could not deactivate requested controllers")
@@ -294,6 +307,8 @@ class PhoebeEstopManager(Node):
         switch_controller_request.timeout.nanosec = int(0.5e9)  # 0.5s
 
         # turn off freedrive controllers if they are started
+        if not self.list_controllers_client.wait_for_service(0.05):
+            return
         list_controllers_request = ListControllers.Request()
         list_controllers_response = self.list_controllers_client.call(list_controllers_request)
 
@@ -308,6 +323,9 @@ class PhoebeEstopManager(Node):
             switch_controller_request.activate_controllers = self.stopped_controllers
             switch_controller_request.deactivate_controllers = stop_freedrive_controllers
 
+            if not self.switch_controllers_client.wait_for_service(0.1):
+                self.get_logger().warn("Did not restart controllers because the switch_controllers service wasn't found")
+                return
             switch_controllers_response = self.switch_controllers_client.call(switch_controller_request)
             if not switch_controllers_response.ok:
                 self.get_logger().error("Could not activate requested controllers")
@@ -317,6 +335,29 @@ class PhoebeEstopManager(Node):
         # clear stopped controllers for the next round to populate
         self.stopped_controllers = []
 
+    def controller_manager_check(self):
+        node_names = self.get_node_names_and_namespaces()
+        last_is_cm_active = self.is_cm_active
+        for name in node_names:
+            if "controller_manager" in name:
+                # log that we are active, and let the user know that 
+                self.is_cm_active = True
+                if not last_is_cm_active:
+                    self.get_logger().info("Found the controller manager! Managing estop safety")
+
+                # once we have run this one time, we are good to run the main loop
+                self.ready_to_start = True
+                return
+
+        # if we made it all the way through without finding the cm, set it to false
+        self.is_cm_active = False
+
+        # let the user know that we aren't handling controllers if this is a new state
+        if not self.ready_to_start or last_is_cm_active:
+            self.get_logger().warn("Can't find the controller manager. Not handling controllers for now.")
+
+        # once we have run this one time, we are good to run the main loop
+        self.ready_to_start = True
 
 def main(args=None):
     rclpy.init(args=args)
