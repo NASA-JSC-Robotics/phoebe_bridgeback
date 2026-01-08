6dd762ce7affeca70bbe44b21dd5a46728872283
Erik Holum <erik.holum@nasa.gov>
2026-01-08 16:26:09 -0500

    Add point cloud republishers for the left and right cameras

--- phoebe_mujoco_config/launch/phoebe_mujoco.launch.py
+++ phoebe_mujoco_config/launch/phoebe_mujoco.launch.py
@@ -22,6 +22,7 @@ import os
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
 
 from ament_index_python.packages import get_package_share_directory
 
@@ -57,4 +58,34 @@ def generate_launch_description():
         }.items(),
     )
 
-    return LaunchDescription([control_launch, teleop_launch])
+    left_point_cloud_proc = Node(
+        package="depth_image_proc",
+        executable="point_cloud_xyzrgb_node",
+        name="left_point_cloud_proc_node",
+        parameters=[{
+            "use_sim_time": True,
+        }],
+        remappings=[
+            ('rgb/image_rect_color', '/left_wrist_mounted_camera_/color/image_raw'),
+            ('rgb/camera_info', '/left_wrist_mounted_camera_/color/camera_info'),
+            ('depth_registered/image_rect', '/left_wrist_mounted_camera_/aligned_depth_to_color/image_raw'),
+            ('points', '/left_wrist_mounted_camera_/depth/color/points'),
+        ],
+    )
+
+    right_point_cloud_proc = Node(
+        package="depth_image_proc",
+        executable="point_cloud_xyzrgb_node",
+        name="right_point_cloud_proc_node",
+        parameters=[{
+            "use_sim_time": True,
+        }],
+        remappings=[
+            ("rgb/image_rect_color", "/right_wrist_mounted_camera_/color/image_raw"),
+            ("rgb/camera_info", "/right_wrist_mounted_camera_/color/camera_info"),
+            ("depth_registered/image_rect", "/right_wrist_mounted_camera_/aligned_depth_to_color/image_raw"),
+            ("points", "/right_wrist_mounted_camera_/depth/color/points"),
+        ],
+    )
+
+    return LaunchDescription([control_launch, teleop_launch, left_point_cloud_proc, right_point_cloud_proc])
