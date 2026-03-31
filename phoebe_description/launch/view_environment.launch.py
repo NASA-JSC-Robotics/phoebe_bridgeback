ebefeabac000a128828e5f211496befac146a50a
Zarrin Bashir <zarrin.t.bashir@nasa.gov>
2026-03-31 16:07:49 -0500

    Replaced cylinder jig mesh obj file with stl file in the environment urdf. Plus general clean up and pre-commit.

--- phoebe_description/launch/view_environment.launch.py
+++ phoebe_description/launch/view_environment.launch.py
@@ -19,9 +19,8 @@
 
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
+from launch.substitutions import Command, FindExecutable, PathJoinSubstitution
 from launch_ros.substitutions import FindPackageShare
-
 
 
 def generate_launch_description():
@@ -36,9 +35,7 @@ def generate_launch_description():
     )
     robot_description = {"robot_description": robot_description_content}
 
-    
     rviz_config_file = PathJoinSubstitution([FindPackageShare("phoebe_description"), "rviz", "view_environment.rviz"])
-
 
     robot_state_publisher = Node(
         package="robot_state_publisher",
