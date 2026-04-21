f0dda193026023c95c6032cd7448a4bb3d9abbc3
Erik Holum <erik.holum@nasa.gov>
2026-04-21 10:42:42 -0500

    Cleanup keyboard

--- phoebe_deploy/scripts/keyboard_joy.py
+++ phoebe_deploy/scripts/keyboard_joy.py
@@ -17,39 +17,47 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-
-import termios
 import select
 import sys
+import termios
+import threading
 import time
 import tty
-import threading
 
 import rclpy
-from rclpy.node import Node
 from geometry_msgs.msg import TwistStamped
+from rclpy.node import Node
+
+LINEAR_SPEED = 0.1
+ANGULAR_SPEED = 0.1
+
+PUBLISH_PERIOD = 0.1
+KEY_POLL_PERIOD = 0.02
+# This is a bit annoying but bumping this up to avoid the OS key dedupe limit
+KEY_TIMEOUT = 0.6
+
+ARROW_UP = "[A"
+ARROW_DOWN = "[B"
+ARROW_RIGHT = "[C"
+ARROW_LEFT = "[D"
 
 
-# Simple script for converting keyboard key input to a twist message for driving the bridgeback
 class KeyboardTeleopNode(Node):
-
     def __init__(self):
         super().__init__("phoebe_teleop")
 
-        # Publish twist commands at 10 hz
         self.twist = TwistStamped()
-        self.last_key_time = time.time() - 0.2
         self.reset_twist()
-        self.publisher_ = self.create_publisher(TwistStamped, "/cmd_vel", 1)
-        self.timer = self.create_timer(0.1, self.publish_twist)
+        self.last_key_time = time.time() - KEY_TIMEOUT * 2
         self.running = True
 
-        self.key_thread = threading.Thread(target=self.key_listener_loop)
-        self.key_thread.daemon = True
+        self.publisher_ = self.create_publisher(TwistStamped, "/cmd_vel", 1)
+        self.timer = self.create_timer(PUBLISH_PERIOD, self.publish_twist)
+
+        self.key_thread = threading.Thread(target=self.key_listener_loop, daemon=True)
         self.key_thread.start()
 
-        # For configuring the terminal to go silent, but saving the state so that we
-        # can reset the terminal to echo key input when the script exits
+        # Save terminal settings so we can restore echo on exit
         self.fd_ = sys.stdin
         self.old_settings_ = termios.tcgetattr(self.fd_)
         self.old_settings_[3] = self.old_settings_[3] | termios.ECHO
@@ -64,51 +72,52 @@ class KeyboardTeleopNode(Node):
         if self.key_thread.is_alive():
             self.key_thread.join()
 
-        # Do the ROS shutdown
         super().destroy_node()
 
     def reset_twist(self):
         self.twist.twist.linear.x = 0.0
         self.twist.twist.angular.z = 0.0
 
+    def set_twist(self, linear, angular):
+        self.twist.twist.linear.x = linear
+        self.twist.twist.angular.z = angular
+
     def key_listener_loop(self):
         # Configures the shell to read raw input
         tty.setcbreak(sys.stdin)
 
         while self.running:
-            # Check for key presses, if nothing reloop at 50 hz
-            rlist, _, _ = select.select([sys.stdin], [], [], 0.02)
+            # Poll for a key; if none, loop again at requested period
+            rlist, _, _ = select.select([sys.stdin], [], [], KEY_POLL_PERIOD)
             if not rlist:
                 continue
 
-            # Check for arrow key input
             key = sys.stdin.read(1)
             if key == "\x1b":
+                # Arrow keys arrive as ESC + 2 bytes
                 self.last_key_time = time.time()
-                key = sys.stdin.read(2)
-                if key == "[A":  # Up arrow
-                    self.twist.twist.linear.x = 0.1
-                    self.twist.twist.angular.z = 0.0
-                elif key == "[B":  # Down arrow
-                    self.twist.twist.linear.x = -0.1
-                    self.twist.twist.angular.z = 0.0
-                elif key == "[C":  # Right arrow
-                    self.twist.twist.linear.x = 0.0
-                    self.twist.twist.angular.z = -0.1
-                elif key == "[D":  # Left arrow
-                    self.twist.twist.linear.x = 0.0
-                    self.twist.twist.angular.z = 0.1
+                seq = sys.stdin.read(2)
+                if seq == ARROW_UP:
+                    self.set_twist(LINEAR_SPEED, 0.0)
+                elif seq == ARROW_DOWN:
+                    self.set_twist(-LINEAR_SPEED, 0.0)
+                elif seq == ARROW_RIGHT:
+                    self.set_twist(0.0, -ANGULAR_SPEED)
+                elif seq == ARROW_LEFT:
+                    self.set_twist(0.0, ANGULAR_SPEED)
             else:
                 # Stop the robot on any other key
                 self.reset_twist()
 
     def publish_twist(self):
-        if self.running:
-            # If we haven't received a command in 100 ms stop the robot
-            if time.time() - self.last_key_time > 0.1:
-                self.reset_twist()
+        if not self.running:
+            return
 
-            self.publisher_.publish(self.twist)
+        # Stop the robot if we haven't seen a recent arrow-key press
+        if time.time() - self.last_key_time > KEY_TIMEOUT:
+            self.reset_twist()
+
+        self.publisher_.publish(self.twist)
 
 
 def main(args=None):
