7fdd030246dc97e0477b192e3807547678a357a6
Nathan Dunkelberger <nathan.b.dunkelberger@nasa.gov>
2026-05-05 12:56:13 -0500

    fixing generate_mjcf to include delay inside opaque function

--- phoebe_mujoco_config/launch/generate_mjcf.launch.py
+++ phoebe_mujoco_config/launch/generate_mjcf.launch.py
@@ -28,9 +28,8 @@ from launch.substitutions import (
     PathJoinSubstitution,
 )
 from launch_ros.substitutions import FindPackageShare
-from launch.event_handlers import OnShutdown
+from launch.event_handlers import OnShutdown, OnProcessExit
 from launch.actions import RegisterEventHandler, DeclareLaunchArgument, OpaqueFunction
-from launch.conditions import IfCondition, UnlessCondition
 
 
 def generate_launch_description():
@@ -41,6 +40,7 @@ def generate_launch_description():
         DeclareLaunchArgument(
             "use_pregenerated_assets_dir",
             default_value="false",
+            choices=["true", "false"],
             description="Use pre-generated assets dir. This is useful if you are just modifying an existing structure",
         )
     )
@@ -48,6 +48,7 @@ def generate_launch_description():
         DeclareLaunchArgument(
             "use_left_static_pedestal",
             default_value="false",
+            choices=["true", "false"],
             description="Replace left ewellix lift kit with static pedestal.",
         )
     )
@@ -76,15 +77,6 @@ def generate_launch_description():
         ]
     )
 
-    # this version can be run for general
-    # make_mjcf_from_robot_description = Node(
-    #     package="mujoco_ros2_control",
-    #     executable="make_mjcf_from_robot_description.py",
-    #     output="screen",
-    #     arguments=default_arguments,
-    #     condition=UnlessCondition(LaunchConfiguration("use_pregenerated_assets_dir")),
-    # )
-
     def launch_mjcf_node(context):
         tmp = tempfile.NamedTemporaryFile(mode="w", suffix=".urdf", delete=False)
         tmp.write(robot_description_content.perform(context))
@@ -94,14 +86,34 @@ def generate_launch_description():
             "--urdf",
             tmp.name,
             "-c",  # convert stl to obj
-            # "-f",  # first link fixed --> free
             "--save_only",
         ]
 
-        args_with_assets_dir = default_arguments + [
-            "--asset_dir",
-            PathJoinSubstitution([FindPackageShare(phoebe_mujoco_package_name), "description", "assets"]),
-        ]
+        if use_pregenerated_assets_dir.perform(context) == "true":
+            default_arguments = default_arguments + [
+                "--asset_dir",
+                PathJoinSubstitution([FindPackageShare(phoebe_mujoco_package_name), "description", "assets"]),
+            ]
+
+        generate_mjcf = Node(
+            package="mujoco_ros2_control",
+            executable="make_mjcf_from_robot_description.py",
+            output="both",
+            emulate_tty=True,
+            arguments=default_arguments,
+        )
+
+        post_process_mjcf = Node(
+            package="phoebe_mujoco_config",
+            executable="post_process_mjcf.py",
+            output="screen",
+        )
+
+        wheel_code_gen = Node(
+            package="phoebe_mujoco_config",
+            executable="wheel_code_gen.py",
+            output="screen",
+        )
 
         # Ensure the file gets deleted
         def cleanup(event, context):
@@ -109,53 +121,17 @@ def generate_launch_description():
                 os.remove(tmp.name)
 
         return [
-            # Writing to /mujoco_robot_description_preprocessed instead of /mujoco_robot_description because the
-            # we need to do some post-processing before we can actually use the output. See node below for more details
-            Node(
-                package="mujoco_ros2_control",
-                executable="make_mjcf_from_robot_description.py",
-                output="both",
-                emulate_tty=True,
-                arguments=default_arguments,
-                condition=UnlessCondition(use_pregenerated_assets_dir),
-            ),
-            Node(
-                package="mujoco_ros2_control",
-                executable="make_mjcf_from_robot_description.py",
-                output="both",
-                emulate_tty=True,
-                arguments=args_with_assets_dir,
-                condition=IfCondition(use_pregenerated_assets_dir),
-            ),
-            # This waits for the topic /mujoco_robot_description_preprocessed to be published, then takes the data,
-            # post-processes it, and writes out the modified data to /mujoco_robot_description. This handles some
-            # special components like wheels, custom robotiq gripper mujoco representations, april tags, etc
+            generate_mjcf,
+            wheel_code_gen,
+            RegisterEventHandler(OnProcessExit(target_action=generate_mjcf, on_exit=[post_process_mjcf])),
             RegisterEventHandler(OnShutdown(on_shutdown=cleanup)),
         ]
 
     generate_mjcf = OpaqueFunction(function=launch_mjcf_node)
 
-    post_process_mjcf = Node(
-        package="phoebe_mujoco_config",
-        executable="post_process_mjcf.py",
-        output="screen",
-    )
-
-    wheel_code_gen = Node(
-        package="phoebe_mujoco_config",
-        executable="wheel_code_gen.py",
-        output="screen",
-    )
-
-    # delay_post_process = RegisterEventHandler(
-    #     OnProcessExit(target_action=generate_mjcf, on_exit=[post_process_mjcf])
-    # )
-
     return LaunchDescription(
         declared_arguments
         + [
             generate_mjcf,
-            post_process_mjcf,
-            wheel_code_gen,
         ]
     )
