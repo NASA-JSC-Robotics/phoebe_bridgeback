c50d9066baadc0f1d6d76ae4aa06b675d86192aa
Andrea Vu <andrea.l.vu@nasa.gov>
2026-07-15 13:26:53 -0500

    adding the friction controller parameters for torque control on phoebe's left arm

--- phoebe_deploy/launch/spawn_controllers/spawn_controllers_ur.launch.py
+++ phoebe_deploy/launch/spawn_controllers/spawn_controllers_ur.launch.py
@@ -83,5 +83,6 @@ def generate_launch_description():
     nodes.append(MakeControllerNode("left_ur_joint_trajectory_controller"))
     nodes.append(MakeControllerNode("left_freedrive_mode_controller", active=False, condition=UnlessCondition(is_sim)))
     nodes.append(MakeControllerNode("left_forward_effort_controller", active=False))
+    nodes.append(MakeControllerNode("left_friction_model_controller", active=False))
 
     return LaunchDescription(declared_arguments + nodes)
