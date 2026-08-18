94578b2b7dc8c5aa9073dc751d3ce6ba73c16685
Mark Paterson <mark.paterson@nasa.gov>
2026-08-18 14:33:58 -0500

    WIP: Comm launch with proton working

--- phoebe_deploy/launch/ridgeback_comm.launch.py
+++ phoebe_deploy/launch/ridgeback_comm.launch.py
@@ -19,12 +19,14 @@
 
 
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument, ExecuteProcess, GroupAction
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument, GroupAction
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import FindExecutable, PathJoinSubstitution, LaunchConfiguration
+from launch.substitutions import PathJoinSubstitution, LaunchConfiguration
 from launch_ros.actions import Node, PushRosNamespace
 from launch_ros.substitutions import FindPackageShare
 from launch_ros.parameter_descriptions import ParameterFile
+# import logging
+# logging.root.setLevel(logging.DEBUG)
 
 
 def generate_launch_description():
@@ -58,18 +60,37 @@ def generate_launch_description():
     pkg_clearpath_ros2_socketcan_interface = FindPackageShare("clearpath_ros2_socketcan_interface")
     pkg_clearpath_ros2_socketcan_interface = FindPackageShare("clearpath_ros2_socketcan_interface")
     pkg_clearpath_diagnostics = FindPackageShare("clearpath_diagnostics")
+    pkg_clearpath_firmware = FindPackageShare("clearpath_firmware")
+    pkg_proton = FindPackageShare("proton_ros2")
 
     # config files
     config_can = PathJoinSubstitution([pkg_phoebe_deploy, "config", "ridgeback", "can_config.yaml"])
     setup_path = PathJoinSubstitution([pkg_phoebe_deploy, "config", "ridgeback"])
-    analyzer_params = PathJoinSubstitution([pkg_clearpath_diagnostics, "config", "diagnostics.yaml"])
 
     # Declare launch files
+    launch_arg_diagnostic_updater_params = DeclareLaunchArgument(
+        'diagnostic_updater_params',
+        default_value=PathJoinSubstitution([pkg_phoebe_deploy, "config", "ridgeback", "diagnostic_updater.yaml"]),
+        description='')
+
+    diagnostic_updater_params = LaunchConfiguration('diagnostic_updater_params')
+
+    launch_arg_diagnostic_aggregator_params = DeclareLaunchArgument(
+        'diagnostic_aggregator_params',
+        default_value=PathJoinSubstitution([pkg_phoebe_deploy, "config", "ridgeback", "diagnostic_aggregator.yaml"]),
+        description='')
+
+    diagnostic_aggregator_params = LaunchConfiguration('diagnostic_aggregator_params')
+
+
     launch_file_receiver = PathJoinSubstitution(
         [pkg_clearpath_ros2_socketcan_interface, "launch", "receiver.launch.py"]
     )
     launch_file_sender = PathJoinSubstitution([pkg_clearpath_ros2_socketcan_interface, "launch", "sender.launch.py"])
     launch_file_phoebe_safety = PathJoinSubstitution([pkg_phoebe_safety, "launch", "phoebe_safety_manager.launch.py"])
+    launch_file_proton = PathJoinSubstitution([pkg_proton, "launch", "proton_ros2.launch.py"])
+    launch_file_diagnostics = PathJoinSubstitution([pkg_clearpath_diagnostics, 'launch', 'diagnostics.launch.py'])
+
 
     launch_receiver = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([launch_file_receiver]),
@@ -90,6 +111,23 @@ def generate_launch_description():
     launch_phoebe_safety = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([launch_file_phoebe_safety]),
     )
+    launch_proton = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([launch_file_proton]),
+        launch_arguments={
+            "config_file": PathJoinSubstitution([pkg_clearpath_firmware, "proton", "r100.yaml"]),
+            "target": "pc",
+            "namespace": default_ns
+        }.items(),
+    )
+
+    # launch_diagnostics = IncludeLaunchDescription(
+    #     PythonLaunchDescriptionSource([launch_file_diagnostics]),
+    #     launch_arguments={
+    #             'namespace': default_ns,
+    #             'updater_parameters': diagnostic_updater_params,
+    #             'aggregator_parameters': diagnostic_aggregator_params,
+    #     }
+    # )
 
     # Nodes
     node_wireless_watcher = Node(
@@ -114,6 +152,7 @@ def generate_launch_description():
         package="clearpath_hardware_interfaces",
         namespace=default_ns,
         output="screen",
+        parameters=[{ 'tf_prefix': tf_prefix }],
         arguments=[
             "-s",
             setup_path,
@@ -126,24 +165,11 @@ def generate_launch_description():
         package="clearpath_hardware_interfaces",
         namespace=default_ns,
         output="screen",
+        parameters=[{ 'tf_prefix': tf_prefix }],        
         arguments=[
             "-s",
             setup_path,
         ],
-    )
-
-    node_micro_ros_agent = Node(
-        name="micro_ros_agent",
-        executable="micro_ros_agent",
-        package="micro_ros_agent",
-        namespace=default_ns,
-        output="screen",
-        arguments=[
-            "udp4",
-            "--port",
-            "11411",
-        ],
-        prefix="taskset -c 4",
     )
 
     node_lighting_node = Node(
@@ -174,68 +200,55 @@ def generate_launch_description():
         ],
     )
 
-    node_aggregator_node = Node(
-        package="diagnostic_aggregator",
-        executable="aggregator_node",
-        namespace=default_ns,
-        output="screen",
-        parameters=[analyzer_params],
-        remappings=[
-            ("/diagnostics", "diagnostics"),
-            ("/diagnostics_agg", "diagnostics_agg"),
-            ("/diagnostics_toplevel_state", "diagnostics_toplevel_state"),
-        ],
-    )
+    # node_aggregator_node = Node(
+    #     package="diagnostic_aggregator",
+    #     executable="aggregator_node",
+    #     namespace=default_ns,
+    #     output="screen",
+    #     parameters=[analyzer_params],
+    #     remappings=[
+    #         ("/diagnostics", "diagnostics"),
+    #         ("/diagnostics_agg", "diagnostics_agg"),
+    #         ("/diagnostics_toplevel_state", "diagnostics_toplevel_state"),
+    #     ],
+    # )
 
-    node_diagnostics_updater = Node(
-        package="clearpath_diagnostics",
-        executable="diagnostics_updater",
-        namespace=default_ns,
-        output="screen",
-        remappings=[
-            ("/diagnostics", "diagnostics"),
-            ("/diagnostics_agg", "diagnostics_agg"),
-            ("/diagnostics_toplevel_state", "diagnostics_toplevel_state"),
-        ],
-        arguments=["-s", setup_path],
-    )
+    # node_diagnostics_updater = Node(
+    #     package="clearpath_diagnostics",
+    #     executable="diagnostics_updater",
+    #     namespace=default_ns,
+    #     output="screen",
+    #     remappings=[
+    #         ("/diagnostics", "diagnostics"),
+    #         ("/diagnostics_agg", "diagnostics_agg"),
+    #         ("/diagnostics_toplevel_state", "diagnostics_toplevel_state"),
+    #     ],
+    #     arguments=["-s", setup_path],
+    # )
 
-    # Processes
-    # note this will be incorrectly namespaced if a namespace is pushed for this file
-    # this should be converted to a node so it picks up the namespace
-    # IMPORTANT: The ROS_DOMAIN_ID must be 0 when calling the service to match the MCU's
-    # initial ROS_DOMAIN_ID. Temporarily override it here just for that purpose.
-    process_configure_mcu = ExecuteProcess(
-        shell=True,
-        cmd=[
-            ["export ROS_DOMAIN_ID=0;"],
-            [
-                FindExecutable(name="ros2"),
-                " service call platform/mcu/configure",
-                " clearpath_platform_msgs/srv/ConfigureMcu",
-                ' "{domain_id: 23,',
-                f" robot_namespace: '{default_ns}'}}\"",
-            ],
-        ],
-    )
+    # launch_args = [
+    #     launch_arg_diagnostic_aggregator_params,
+    #     launch_arg_diagnostic_updater_params
+    # ]
 
     launches = [
         launch_receiver,
         launch_sender,
         launch_phoebe_safety,
+        launch_proton,
+        # launch_diagnostics
     ]
     nodes = [
         node_wireless_watcher,
-        node_battery_state_estimator,
-        node_battery_state_control,
-        node_micro_ros_agent,
+        # Comment out pending addressing the $(var stuff) in robot.yaml.
+        # node_battery_state_estimator,
+        # node_battery_state_control,
         node_lighting_node,
         node_puma_control,
-        node_aggregator_node,
-        node_diagnostics_updater,
     ]
-    processes = [process_configure_mcu]
+    processes = []
 
     ns_action = GroupAction(actions=[PushRosNamespace(namespace)] + launches + nodes + processes)
+    # ns_action = GroupAction(actions=[PushRosNamespace(namespace)] + launch_args + launches + nodes + processes)
 
     return LaunchDescription(declared_arguments + [ns_action])
